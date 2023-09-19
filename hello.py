c4c374c0fd8d667b0a4b240bda969bc988ec06e6
Guozhen Ding <mail@guozhen.dev>
2023-09-19 19:18:26 -0400

    Activity 4, Add the support for the form using WTForm. This reproduced the example 4.7

--- hello.py
+++ hello.py
@@ -1,17 +1,35 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Regexp
 from flask_moment import Moment
 from datetime import datetime
+import uuid
 
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 app = Flask(__name__)
+app.config['SECRET_KEY'] = str(uuid.uuid4())
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', 
+                            name=session.get('name'), 
+                            form=form,
+                            email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
