77875dbdd6977e9ad01640aeec0086bc81b08d38
Guozhen Ding <mail@guozhen.dev>
2023-09-19 19:20:57 -0400

    Activity4, Add the support for user to submit their UofT email, and including the screenshots for the project.

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky.
 ![Screenshot for Activity2](https://github.com/guozhen-dev/pics_bed/blob/main/ECE444_L1_activity2.png?raw=true)
 ## Activity 3
 ![Screenshot for Activity3](https://github.com/guozhen-dev/pics_bed/blob/main/ECE444_L1_activity3.png?raw=true)
-
+## Activity 4
+![Screenshot for Activity3](https://github.com/guozhen-dev/pics_bed/blob/main/ECE444_L1_activity4_1.png?raw=true)
+![Screenshot for Activity3](https://github.com/guozhen-dev/pics_bed/blob/main/ECE444_L1_activity4_2.png?raw=true)
+![Screenshot for Activity3](https://github.com/guozhen-dev/pics_bed/blob/main/ECE444_L1_activity4_3.png?raw=true)
+![Screenshot for Activity3](https://github.com/guozhen-dev/pics_bed/blob/main/ECE444_L1_activity4_4.png?raw=true)
--- hello.py
+++ hello.py
@@ -10,6 +10,7 @@ import uuid
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = str(uuid.uuid4())
@@ -21,9 +22,29 @@ moment = Moment(app)
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        # First, we check if the info entered by user is 
+        # differ from the previous saved one.
         old_name = session.get('name')
+        old_email = session.get('email')
+        # If not, we issue the warning messages. 
         if old_name and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
+        # Now we validate if the email provided is a UofT 
+        # email address.
+        # NOTE: This validation is NOT accurate, but it is 
+        # enough to meet the requirement(i.e. 'utoronto' is 
+        # a substring of the email). 
+        if 'utoronto' in form.email.data:
+            session['email'] = form.email.data
+        else:
+            # If we got an invalid email, we lable it and 
+            # later we can ask for a UofT email.
+            session['email'] = 'INVALID'
+        
+        # User can update their name unconditionally.
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', 
